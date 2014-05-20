ea32f9d229e9b2cb28c45cfb23577f7e5ede16ce
Trevor Jim <tjim@mac.com>
2014-05-20 10:30:39 -0400

    Print ModuleID in echo, for LLVM comparison

--- compiler/echo.ml
+++ compiler/echo.ml
@@ -1,9 +1,9 @@
-let ch =
+let ch, module_id =
   if Array.length Sys.argv > 1 then
-    open_in Sys.argv.(1)
+    open_in Sys.argv.(1), Sys.argv.(1)
   else
-    stdin
+    stdin, "<stdin>"
 
 let cu = Lllex.parse ch
 ;;
-Printf.printf "%s" (Util.spr Util.bpr_cu cu);;
+Printf.printf "; ModuleID = '%s'\n%s" module_id (Util.spr Util.bpr_cu cu);;
