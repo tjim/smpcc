b1651c6b988b27128dc0f02af709173f8ed02c5d
Yevgeniy Vahlis <evahlis@gmail.com>
2015-03-09 13:48:59 -0400

    added ECC example

---
+++ examples/eccmpc.c
@@ -0,0 +1,38 @@
+/* Second price (Vickrey) auction
+   To compile
+       smpcc vickrey.c -circuitlib gmw
+   To run
+       go run *.go 4 5 6
+   where 4, 5, 6 are the bids -- choose your own
+
+   To test the C code
+       clang vickrey.c -DTEST
+       ./a.out
+*/
+#include <stdio.h>
+#include "uECC.h"
+
+extern unsigned int input(unsigned int);
+extern unsigned int num_peers();
+
+int main() {
+  unsigned int cur_max = input(0);
+  unsigned char private_key[32] = {0}; 
+  uint8_t p_hash[uECC_BYTES] = {0};
+  uint8_t p_signature[uECC_BYTES*2];
+
+  for (unsigned int i = 1; i < num_peers(); i++) {
+    unsigned char val = input(i);
+    private_key[0] ^= val;
+  }
+  uECC_sign(private_key, p_hash, p_signature);
+
+  printf("Signature:\n");
+  for (unsigned int i = 0; i < 64; i++)
+  {
+    printf("%x ", p_signature[i]);
+  }
+  printf("\n");
+  
+  return 0;
+}
