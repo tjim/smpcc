238b56fcf6a3ff6e6358607fd27dce72b2d53ebb
Trevor Jim <tjim@mac.com>
2015-03-26 18:17:17 -0400

    Improve ecc example

--- examples/uECC/ecc-example.c
+++ examples/uECC/ecc-example.c
@@ -1,26 +1,28 @@
-/* Second price (Vickrey) auction
-   To compile
-       smpcc vickrey.c -circuitlib gmw
-   To run
-       go run *.go 4 5 6
-   where 4, 5, 6 are the bids -- choose your own
+/* ecc signature example */
+/* In this directory do
 
-   To test the C code
-       clang vickrey.c -DTEST
-       ./a.out
+       gcc -U __SIZEOF_INT128__ -E ecc-example.c > ../ecc.c
+
+   to obtain a single C file that you can compile with smpcc.
+
+   Here the "-U __SIZEOF_INT128__" causes uECC.c to be compiled
+   without support for 128 bit ints, which CIL cannot handle.
+
+   You must use gcc on Linux b/c on Mac OS X you will encounter CIL
+   errors.
 */
+
 #include <stdio.h>
-#include "uECC.h"
+#include "uECC.c"
 
 extern unsigned int input(unsigned int);
 extern unsigned int num_peers();
 
-int main() {
-  unsigned int cur_max = input(0);
-  unsigned char private_key[32] = {0}; 
-  uint8_t p_hash[uECC_BYTES] = {0};
-  uint8_t p_signature[uECC_BYTES*2];
+unsigned char private_key[32];
+uint8_t p_hash[uECC_BYTES];
+uint8_t p_signature[uECC_BYTES*2];
 
+int main() {
   for (unsigned int i = 1; i < num_peers(); i++) {
     unsigned char val = input(i);
     private_key[0] ^= val;
